6c169aa7d187ff2e7f60bc1bbfa33c97153effa7
MyVanBui <myvanbui6112000@gmail.com>
2021-12-02 10:56:18 +0700

    update giao diện trang Phim Admin

--- WebsiteDatVeXemPhim/WebsiteDatVeXemPhim/Scripts/Movie/Phim.js
+++ WebsiteDatVeXemPhim/WebsiteDatVeXemPhim/Scripts/Movie/Phim.js
@@ -1,13 +1,54 @@
-﻿loadData(){
-    $ajax({
-        url:"https://localhost:44382/api/Movie/Phim",
-        method:"GET",
-        data:"",
-        contentType:"application/json",
-        dataType:""
-    }).done(function (response){
+﻿var vmListFilm = new Vue({
+    el: "#ListFilm",
+    data: {
+        currentPage: 1,
+        pageSize: 10,
+        listPageSize: [10, 25, 50],
+        pageCount: 1,
+        pageView: "",
+        txtSearch: '',
+        Status: -1,
+        list: {},
+        textFilter: "Tất cả",
+        CountAllFilm: 0,
+        VName: "",
+        RoleId: -1
+    },
 
-    }).fail(function(response){
+    method: {
+        getListKOL_Campaign: function () {
+            AddLoader();
+            var self = this;
+            var modal = {
+                //Search: self.txtSearch.trim(),
+                PageSize: self.pageSize,
+                CurrentPage: self.currentPage - 1,
+                Status: self.Status
+            }
+            $.ajax({
+                data: modal,
+                url: "/api/MovieApi/loadListPhim",
+                type: 'POST',
+                dataType: 'json',
+                contentType: "application/x-www-form-urlencoded; charset=UTF-8"
+            }).then(res => {
+                self.list = res.data.list;
+                self.totalPage = res.totalPage;
+                self.pageView = res.data.PageView;
+                HiddenLoader();
+                $("#ListFilm").css("display", "block");
+                setTimeout(function () {
+                    $(function () {
+                        $('[data-toggle="tooltip"]').tooltip(
+                            {
+                                container: 'body'
+                            }
+                        );
+                    });
+                });
+            });
+        },
 
-    })
-}
+    }
+})
+
