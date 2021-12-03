b0d4ce8dca272256f1d90d3e8cb5a75c687cd562
quynh1007 <77844684+quynh1007@users.noreply.github.com>
2021-12-04 00:17:40 +0700

    update CustommerAPI hàm thêm sửa

---
+++ WebsiteDatVeXemPhim/WebsiteDatVeXemPhim/Partials/Ve.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteDatVeXemPhim.EF
+{
+    public partial class Ve
+    {
+        public enum TK
+        {
+            daban = 0,
+            chuaban = 1,
+        }
+    }
+}
