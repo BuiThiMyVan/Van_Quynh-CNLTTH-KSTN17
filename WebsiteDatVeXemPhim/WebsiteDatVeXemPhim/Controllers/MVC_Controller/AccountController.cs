56bf4c73c2dde76a2ad09046ca7cb5e9c3af5fa9
MyVanBui <myvanbui6112000@gmail.com>
2021-12-03 14:58:06 +0700

    tao giao dien Login

---
+++ WebsiteDatVeXemPhim/WebsiteDatVeXemPhim/Controllers/MVC_Controller/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteDatVeXemPhim.Controllers.MVC_Controller
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [System.Web.Http.AcceptVerbs("POST", "GET")]
+        public ActionResult Login()
+        {
+            //    ADMIN admin = new ADMIN();
+            //    model.Password = admin.EncodePassword(model.Password);
+            //    var res = con.ADMINs.Where(a => a.Username == model.Username && a.Password == model.Password).SingleOrDefault();
+            //    var res_username = con.ADMINs.Where(a => a.Username == model.Username).SingleOrDefault();
+            //    var result = 0;
+
+            //    //Login successfully
+            //    if (res != null)
+            //    {
+            //        //Set Session
+            //        var adminSession = new ADMIN();
+            //        adminSession.Username = res.Username;
+            //        adminSession.Id = res.Id;
+            //        adminSession.Password = res.Password;
+            //        Session.Add(CommonConstants.USER_SESSION, adminSession);
+            //        result = 1;
+            //    }
+            //    else
+            //    {
+            //        if (res_username != null)
+            //        {
+            //            //Account doesn't exist
+            //            result = 3;
+            //        }
+            //        else
+            //        {
+            //            //Fill wrong password
+            //            result = 2;
+            //        }
+            //    }
+            //    return new JsonResult() { Data = result };
+            return View();
+
+        }
+
+        [System.Web.Http.AcceptVerbs("POST", "GET")]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebsiteDatVeXemPhim/WebsiteDatVeXemPhim/Scripts/Account/Login.js
@@ -0,0 +1,66 @@
+﻿var vmLogin = new Vue({
+    el: "#Login",
+    data: {
+        username: '',
+        password: '',
+        error_username: '',
+        error_password: ''
+    },
+    watch: {
+        username: function () {
+            var self = this;
+            if (self.username == '') {
+                self.error_username = "Bạn cần nhập tên đăng nhập";
+            } else {
+                self.error_username = '';
+            }
+        },
+
+        password: function () {
+            var self = this;
+            if (self.password == '') {
+                self.error_password = 'Bạn cần nhập mật khẩu';
+            } else {
+                self.error_password = '';
+            }
+        }
+    },
+
+    methods: {
+        login: function () {
+            var self = this;
+            if (self.error_username == '' || self.error_username == '') {
+                return false;
+            }
+            else {
+                var account = '{Username: "' + username + '", Password: "' + password + '" }';
+                //console.log(account);
+                $.ajax({
+                    type: "POST",
+                    url: "/Account/CheckLogin",
+                    data: account,
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function (response) {
+                        switch (response) {
+                            case 1:
+                                window.location.href = "/Home/Index";
+                                break;
+                            case 2:
+                                error_login.innerText = "Tài khoản không tồn tại";
+                                $("#error-login").show();
+                                break;
+                            case 3:
+                                error_login.innerText = "Mật khẩu nhập không chính xác";
+                                $("#error-login").show();
+                                break;
+                        }
+                    },
+                    error: function () {
+                        alert("Đã xảy ra lỗi trong quá trình đăng nhập!");
+                    }
+                })
+            }
+        }
+    }
+})
